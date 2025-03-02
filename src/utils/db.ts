c882b3c1999f99cf7325f5e4614aacbb37204802
Demiana122 <demianaseed@gmail.com>
2025-03-02 17:16:27 +0200

    second commit

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,19 +1,19 @@
-// import { PrismaClient } from '@prisma/client';
-// const prisma = new PrismaClient();
-// export default prisma;
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+export default prisma;
 
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-    return new PrismaClient()
-}
+// const prismaClientSingleton = () => {
+//     return new PrismaClient()
+// }
 
-declare global {
-    var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
+// declare global {
+//     var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+// }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+// const prisma = globalThis.prisma ?? prismaClientSingleton()
 
-export default prisma
+// export default prisma
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+// if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
