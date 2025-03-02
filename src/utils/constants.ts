3ea748c60517ae030d8cdd304758fbaf9d6976f5
Demiana122 <demianaseed@gmail.com>
2025-03-02 14:33:18 +0200

    first commit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,3 @@
 export const ARTICLE_PER_PAGE = 6;
 
-const PRODUCTION_DOMAIN = "http://localhost:3000";
-const DEVELOPMENT_DOMAIN = "http://localhost:3000";
-
-export const DOMAIN = process.env.NODE_ENV === 'production' 
-    ? PRODUCTION_DOMAIN
-    : DEVELOPMENT_DOMAIN;
+export const DOMAIN = "http://localhost:3000";
